23459c946976e339fcf5386369e3c980ef36391b
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-10-10 19:06:58 -0300

    Create First CLASS, Create First IF Else and While Conditional

---
+++ FirstClass.java
@@ -0,0 +1,25 @@
+public class FirstClass {
+  public static void main(String[] args) {
+    int dadoDoTipoInt = 18;
+    double dadoDoTipoDouble = 10;
+    float dadoDoTipoFloat = 3.14f;
+    String dadoDoTipoString = "Hello";
+    boolean dadoDoTipoBoolear = true;
+    // Long -> Valores muito grandes. Esse precisa de um L grande no final do número
+
+    // if - else
+    if (dadoDoTipoInt == 10) {
+      System.out.println("Entrou no If");
+    } else {
+      System.out.println("entrou no Else");
+    }
+
+    // while
+
+    int valorWhile = 0;
+    while (valorWhile < 3) {
+      System.out.println(valorWhile + "valor inicial é ${valorWhile}");
+      valorWhile++;
+    }
+  }
+}
